ef2183a751d2547a0653bc299a98914be785d3c7
DanielCuevas1822 <daniel_cuevas_hernandez@outlook.com>
2021-04-28 11:24:56 -0500

    Add export to testimoniales actions

--- src/redux/actions/testimonialesActions.js
+++ src/redux/actions/testimonialesActions.js
@@ -1,18 +1,18 @@
-const NEW_TESTIMONIALES = "NEW_TESTIMONIALES";
-const UPDATE_TESTIMONIALES = "UPDATE_TESTIMONIALES";
-const DELETE_TESTIMONIALES = "DELETE_TESTIMONIALES";
+export const NEW_TESTIMONIALES = "NEW_TESTIMONIALES";
+export const UPDATE_TESTIMONIALES = "UPDATE_TESTIMONIALES";
+export const DELETE_TESTIMONIALES = "DELETE_TESTIMONIALES";
 
-const new_testimoniales_action = (payload) => ({
+export const new_testimoniales_action = (payload) => ({
   type: NEW_TESTIMONIALES,
   payload,
 });
 
-const update_testimoniales_action = (payload) => ({
+export const update_testimoniales_action = (payload) => ({
   type: UPDATE_TESTIMONIALES,
   payload,
 });
 
-const delete_testimoniales_action = (payload) => ({
+export const delete_testimoniales_action = (payload) => ({
   type: DELETE_TESTIMONIALES,
   payload,
 });
