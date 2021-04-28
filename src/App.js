f617063fb72d8a482abd7eb487b6b0fddf2cbbe2
DanielCuevas1822 <daniel_cuevas_hernandez@outlook.com>
2021-04-28 13:05:43 -0500

    Connect store to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducers from "./redux/reducers/rootReducers";
+
+import "./styles/main.scss";
 
 import Header from "./components/Header";
 import Loader from "./components/Loader";
@@ -11,11 +16,89 @@ import Blog from "./views/Blog";
 import Testimoniales from "./views/Testimoniales";
 import NotAccess from "./views/NotAccess";
 
-import "./styles/main.scss";
-
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [haveAccess, setHaveAcces] = useState(false);
+  const [initialState, setinitialState] = useState({});
+
+  useEffect(() => {
+    Promise.all([
+      fetch(
+        "https://bienesraices-4eea1-default-rtdb.firebaseio.com/propiedades.json"
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          let propiedadesData = [];
+          for (var clave in data) {
+            let newid = { id: clave };
+            if (data.hasOwnProperty(clave)) {
+              let newinfo = data[clave];
+              let concatInfo = { ...newid, ...newinfo };
+              propiedadesData.push(concatInfo);
+            }
+          }
+          return {
+            propiedades: propiedadesData,
+          };
+        })
+        .catch((err) => {
+          console.log(err);
+        }),
+      fetch("https://bienesraices-4eea1-default-rtdb.firebaseio.com/blog.json")
+        .then((response) => response.json())
+        .then((data) => {
+          let notasData = [];
+          for (var clave in data) {
+            let newid = { id: clave };
+            if (data.hasOwnProperty(clave)) {
+              let newinfo = data[clave];
+              let blogInfo = { ...newid, ...newinfo };
+              notasData.push(blogInfo);
+            }
+          }
+          return {
+            blog: notasData,
+          };
+        })
+        .catch((err) => {
+          console.log(err);
+        }),
+      fetch(
+        "https://bienesraices-4eea1-default-rtdb.firebaseio.com/testimoniales.json"
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          let testimonialesData = [];
+          for (var clave in data) {
+            let newid = { id: clave };
+            if (data.hasOwnProperty(clave)) {
+              let newinfo = data[clave];
+              let testimonialesInfo = { ...newid, ...newinfo };
+              testimonialesData.push(testimonialesInfo);
+            }
+          }
+          return {
+            testimoniales: testimonialesData,
+          };
+        })
+        .catch((err) => {
+          console.log(err);
+        }),
+    ]).then((results) => {
+      let newresults = {};
+      results.map((item) => {
+        newresults = { ...newresults, ...item };
+        return null;
+      });
+      setinitialState(newresults);
+    });
+  }, []);
+
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__();
+
+  const store = createStore(rootReducers, initialState, composeEnhancers);
 
   useEffect(() => {
     const isLogIn = JSON.parse(localStorage.getItem("userinfo"));
@@ -28,38 +111,40 @@ function App() {
   };
 
   return (
-    <div className="App">
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <React.Fragment>
-          {haveAccess ? (
-            <React.Fragment>
-              <Header />
-              <Switch>
-                <Route exact path="/">
-                  <Home />
-                </Route>
-                <Route exact path="/propiedades">
-                  <Propiedades />
-                </Route>
-                <Route exact path="/blog">
-                  <Blog />
-                </Route>
-                <Route exact path="/testimoniales">
-                  <Testimoniales />
-                </Route>
-                <Route exact path="/notaccess">
-                  <NotAccess handleAccess={handleAccess} />
-                </Route>
-              </Switch>
-            </React.Fragment>
-          ) : (
-            <Login handleAccess={handleAccess} />
-          )}
-        </React.Fragment>
-      )}
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <React.Fragment>
+            {haveAccess ? (
+              <React.Fragment>
+                <Header />
+                <Switch>
+                  <Route exact path="/">
+                    <Home />
+                  </Route>
+                  <Route exact path="/propiedades">
+                    <Propiedades />
+                  </Route>
+                  <Route exact path="/blog">
+                    <Blog />
+                  </Route>
+                  <Route exact path="/testimoniales">
+                    <Testimoniales />
+                  </Route>
+                  <Route exact path="/notaccess">
+                    <NotAccess handleAccess={handleAccess} />
+                  </Route>
+                </Switch>
+              </React.Fragment>
+            ) : (
+              <Login handleAccess={handleAccess} />
+            )}
+          </React.Fragment>
+        )}
+      </div>
+    </Provider>
   );
 }
 
