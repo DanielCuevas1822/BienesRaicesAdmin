6a2dc6282c8104876fc486e27e6a28b64edaa6a4
DanielCuevas1822 <daniel_cuevas_hernandez@outlook.com>
2021-05-05 14:04:24 -0500

    New Testimonial

--- src/views/NewTestimonial.jsx
+++ src/views/NewTestimonial.jsx
@@ -1,7 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { useHistory } from "react-router";
 
-const NewTestimonial = () => {
-  return <p>Nuevo testimonial</p>;
+import { new_testimoniales_action } from "../redux/actions/testimonialesActions";
+
+const NewTestimonial = (props) => {
+  const history = useHistory();
+  const { new_testimoniales_action } = props;
+  const [newInfo, setNewInfo] = useState({
+    nombre_testimonial: "Daniel Cuevas",
+    testimonio: "Super",
+  });
+  const handleChange = (e) => {
+    setNewInfo({
+      ...newInfo,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleClick = (e) => {
+    e.preventDefault();
+    const localUserinfo = JSON.parse(localStorage.getItem("userinfo"));
+    if (localUserinfo) {
+      fetch(
+        `https://bienesraices-4eea1-default-rtdb.firebaseio.com/testimoniales.json?auth=${localUserinfo.idToken}`,
+        {
+          method: "POST",
+          body: JSON.stringify(newInfo),
+        }
+      )
+        .then(function (response) {
+          if (response.ok) {
+            return response.json();
+          }
+          return Promise.reject(response);
+        })
+        .then((data) => {
+          const newdata = { ...newInfo, id: data.name };
+          new_testimoniales_action(newdata);
+          history.push("/testimoniales");
+        })
+        .catch(function (error) {
+          console.warn("Something went wrong.", error);
+          history.push("/notaccess");
+        });
+    } else {
+      history.push("/notaccess");
+    }
+  };
+  return (
+    <main>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 list">
+            <form>
+              <div className="form-group">
+                <label htmlFor="nombre_testimonial">Nombre:</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="nombre_testimonial"
+                  name="nombre_testimonial"
+                  value={newInfo.nombre_testimonial}
+                  onChange={handleChange}
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="testimonio">Testimonio:</label>
+                <textarea
+                  className="form-control"
+                  id="testimonio"
+                  rows="10"
+                  name="testimonio"
+                  value={newInfo.testimonio}
+                  onChange={handleChange}
+                ></textarea>
+              </div>
+
+              <button
+                type="submit"
+                className="btn btn-primary"
+                onClick={handleClick}
+              >
+                Guardar
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
-export default NewTestimonial;
+const mapDispatchtoProps = {
+  new_testimoniales_action,
+};
+
+export default connect(null, mapDispatchtoProps)(NewTestimonial);
